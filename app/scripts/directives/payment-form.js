578a523395a9eb8681e6405c83dfa7f4e6a6bd69
Eddy Lane <Naedin@gmail.com>
2013-12-08 19:41:35 +0000

    payment form in directive

---
+++ app/scripts/directives/payment-form.js
@@ -0,0 +1,106 @@
+'use strict';
+
+angular.module('angularStripeTestApp')
+    .directive('paymentForm', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'views/partials/payment-form.html',
+            controller: function ($scope, stripeFactory) {
+
+                /**
+                 * handle response errors
+                 *
+                 * @param error
+                 */
+                var handleError = function (error) {
+
+                        var field;
+
+                        if (!error.param || !$scope.values[error.param]) {
+                            throw new Error('No error or can\'t find response.param in $scope.values');
+                        }
+
+                        field = $scope.values[error.param];
+
+                        $scope.$apply(function () {
+                            field.error = error.message;
+                            field.code = error.code;
+                        });
+                    },
+
+                    /**
+                     * reset all error messages
+                     */
+                        resetErrors = function () {
+                        angular.forEach($scope.values, function (input) {
+                            input.error = false;
+                        });
+                    },
+
+
+                    /**
+                     * save response to database
+                     */
+                        saveToken = function (token) {
+
+                        stripeFactory.pays({
+                            token: token
+                        });
+
+                    };
+
+                /**
+                 * Assign values
+                 *
+                 * @type {{number: {}, cvc: {}, exp_month: {}, exp_year: {}}}
+                 */
+                $scope.values = {
+                    'number': {},
+                    'cvc': {},
+                    'exp_month': {},
+                    'exp_year': {}
+                };
+
+                /**
+                 * Submit the form
+                 *
+                 * @param values
+                 * @returns {boolean}
+                 */
+                $scope.pay = function (values) {
+
+                    $scope.submitted = true;
+
+                    console.log($scope);
+
+                    if ($scope.form.$invalid) {
+                        return false;
+                    }
+
+                    $scope.submitting = true;
+
+                    Stripe.card.createToken({
+
+                        number: values.number.value,
+                        cvc: values.cvc.value,
+                        exp_month: values.exp_month.value,
+                        exp_year: values.exp_year.value
+
+                    }, function (status, response) {
+
+                        $scope.submitting = false;
+                        resetErrors();
+                        if (status === 402) {
+                            handleError(response.error);
+                        } else {
+                            saveToken(response.id);
+                        }
+
+                    });
+
+                    return true;
+                };
+            }
+        };
+    });
